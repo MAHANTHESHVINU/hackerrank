352c3540e51769cf5aebbc45e5cdb5817574f532
vinu <vinumahantheshggowda@gmail.com>
2026-08-19 15:01:31 +0530

    modified some c files

--- 1st.c
+++ 1st.c
@@ -11,12 +11,28 @@
 //     printf("%d\n", c);
 //     return 0;
 // }
-#include <stdio.h>
-#include <math.h>
-double main()
-{
-    int x = 5;
-    int c= ++x + x++ * ++x / ++x % ++x;
-    printf("%d", c);
+// #include <stdio.h>
+// #include <math.h>
+// double main()
+// {
+//     int x = 5;
+//     int c= ++x + x++ * ++x / ++x % ++x;
+//     printf("%d", c);
     
+// }
+// #include <stdio.h>
+// void main()
+// {
+//     int a=2, b=4;
+//     a= ++a + ++a + --b - --b * a-- % --a;
+//     b= a++| a> b && 1;
+//     printf("a=%d, b =%d",a,b);
+// }
+#include <stdio.h>
+void main()
+{
+    int i=0, j=1, k=2, m;
+    m= j-- && i++ || j-- && --k || ++j && --k;
+
+    printf ("%d %d %d %d", i, j, k, m);
 }
---
+++ butterfly.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    // Upper half
+    for (int i = 1; i <= n; i++)
+    {
+        // Left stars
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        // Middle spaces
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            printf(" ");
+        }
+
+        // Right stars
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    // Lower half
+    for (int i = n - 1; i >= 1; i--)
+    {
+        // Left stars
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        // Middle spaces
+        for (int j = 1; j <= 2 * (n - i); j++)
+        {
+            printf(" ");
+        }
+
+        // Right stars
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
