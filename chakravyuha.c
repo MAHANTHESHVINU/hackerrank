54a8d72358f894fe2506c64a9be682b430e65cd3
vinu <vinumahantheshggowda@gmail.com>
2026-08-19 15:12:59 +0530

    added some c files

---
+++ chakravyuha.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 4;
+
+    for (int i = 0; i < 2 * n; i++)
+    {
+        for (int j = 0; j < 2 * n; j++)
+        {
+            int top = i;
+            int left = j;
+            int bottom = 2 * n - 1 - i;
+            int right = 2 * n - 1 - j;
+
+            int min = top;
+
+            if (left < min)
+                min = left;
+
+            if (bottom < min)
+                min = bottom;
+
+            if (right < min)
+                min = right;
+
+            printf("%d", n - min);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
